8f3b8d2eec09fa0a8de87b88b3bd78ecf24f065c
BritikovKI <BritikovKI@yandex.ru>
2022-03-09 11:44:55 +0100

    Tests are finally alive and kicking

---
+++ contracts/test/PrimeToken.sol
@@ -0,0 +1,27 @@
+/*
+
+██████╗░██████╗░██╗███╗░░░███╗███████╗██████╗░░█████╗░░█████╗░
+██╔══██╗██╔══██╗██║████╗░████║██╔════╝██╔══██╗██╔══██╗██╔══██╗
+██████╔╝██████╔╝██║██╔████╔██║█████╗░░██║░░██║███████║██║░░██║
+██╔═══╝░██╔══██╗██║██║╚██╔╝██║██╔══╝░░██║░░██║██╔══██║██║░░██║
+██║░░░░░██║░░██║██║██║░╚═╝░██║███████╗██████╔╝██║░░██║╚█████╔╝
+╚═╝░░░░░╚═╝░░╚═╝╚═╝╚═╝░░░░░╚═╝╚══════╝╚═════╝░╚═╝░░╚═╝░╚════╝░
+
+*/
+// SPDX-License-Identifier: GPL-3.0-or-later
+// solium-disable linebreak-style
+
+pragma solidity 0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+
+contract PrimeToken is ERC20Capped {
+    constructor(
+        uint256 initialSupply,
+        uint256 cap,
+        address genesisMultisig
+    ) public ERC20("PrimeDAO Token", "PRIME") ERC20Capped(cap) {
+        require(initialSupply <= cap); // _mint from ERC20 is not protected
+        ERC20._mint(genesisMultisig, initialSupply);
+    }
+}
