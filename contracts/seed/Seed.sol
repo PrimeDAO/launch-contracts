4f5f8678635c7d6dfb1e41b758abf5e2b7e1f96d
BritikovKI <BritikovKI@yandex.ru>
2022-03-27 13:26:40 +0200

    Noticed there was a code missing

--- contracts/seed/Seed.sol
+++ contracts/seed/Seed.sol
@@ -407,7 +407,6 @@ contract Seed {
             "Seed: distribution haven't started"
         );
         require(!minimumReached, "Seed: minimum funding amount met");
-        ContributorClass class = classes[msg.sender];
         FunderPortfolio storage tokenFunder = funders[msg.sender];
         uint256 fundingAmount = tokenFunder.fundingAmount;
         require(fundingAmount > 0, "Seed: zero funding amount");
