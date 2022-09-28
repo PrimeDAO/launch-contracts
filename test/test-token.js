04e8240dea7a7d965b6673fd120c0ac1dfd3fa10
hdn <hieudn5293@yahoo.de>
2022-09-28 18:29:02 +0200

    save

---
+++ test/test-token.js
@@ -0,0 +1,16 @@
+const { expect } = require("chai");
+
+describe("Token contract", function () {
+  it("Deployment should assign the total supply of tokens to the owner", async function () {
+    const [owner] = await ethers.getSigners();
+
+    const Token = await ethers.getContractFactory("TestToken");
+
+    const hardhatToken = await Token.deploy();
+
+    console.log(owner.address)
+
+    const ownerBalance = await hardhatToken.balanceOf(owner.address);
+    expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
+  });
+});
