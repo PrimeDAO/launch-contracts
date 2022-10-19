37d7e661ae2294ed48d02b90b03db2c324a8f1f9
Zitzak <marvin88gr@gmail.com>
2022-10-19 10:50:55 +0200

    feat: add Signer contract and scripts for delegate
    We need the Signer contract to make use of the delegate -> Safe pattern.
    Next to this I added the scripts to set the delegate, as well as
    the deploy script for the signer contract

---
+++ deploy/01_deploy_signerV2-contract.js
@@ -0,0 +1,31 @@
+const { getSafeAddress } = require("../lib/params/safeAddresses");
+
+const deployFunction = async ({ getNamedAccounts, deployments, ethers }) => {
+  const { deploy } = deployments;
+  const { root } = await getNamedAccounts();
+  const safeAddress = getSafeAddress(root.address);
+
+  const lbpManagerFactoryInstance = await ethers.getContract(
+    "LBPManagerFactory"
+  );
+  const seedFactoryInstance = await ethers.getContract("SeedFactory");
+
+  const deployLBPManagerFunctionSignature =
+    await lbpManagerFactoryInstance.interface.getSighash("deployLBPManager");
+
+  const deploySeedFunctionSignature =
+    await seedFactoryInstance.interface.getSighash("deploySeed");
+
+  await deploy("SignerV2", {
+    from: root,
+    args: [
+      safeAddress,
+      [seedFactoryInstance.address, lbpManagerFactoryInstance.address],
+      [deploySeedFunctionSignature, deployLBPManagerFunctionSignature],
+    ],
+    log: true,
+  });
+};
+
+module.exports = deployFunction;
+module.exports.tags = ["Signer"];
