c73f5c55ab19b267f75e24480cb7ac87e406521c
hdn <hieudn5293@yahoo.de>
2022-11-04 13:39:55 +0100

    dev(tasks): adjust seedManagement

--- tasks/seedManagement.js
+++ tasks/seedManagement.js
@@ -48,37 +48,42 @@ var { SeedArguments } = require("../lib/params/seed-test-args.json");
 task("createSeed", "Creates a Seed directly, bypassing Gnosis safe").setAction(
   async (_, hre) => {
     try {
+      console.log(
+        "------------------------------------------------------------------------------------------"
+      );
       console.log("Creating Seed...");
-      var seedFactoryInstance = await hre.ethers.getContract("SeedFactory");
-      // var seedFactoryInstance = await hre.ethers.getContractAt("SeedFactory", "0x5FbDB2315678afecb367f032d93F642f64180aa3");
+      // var seedFactoryInstance = await hre.ethers.getContract("SeedFactory");
+
+      /* prettier-ignore */ console.log('>>>> _ >>>> ~ file: seedManagement.js ~ line 58 ~ hre.ethers.provider.network', hre.ethers.provider.network)
+
+      var seedFactoryInstance = await hre.ethers.getContractAt("SeedFactory", "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512");
       seedFactoryInstance.address;
       /* prettier-ignore */ console.log('>>>> _ >>>> ~ file: seedManagement.js ~ line 55 ~ seedFactoryInstance.address', seedFactoryInstance.address)
-      console.log(
-        "------------------------------------------------------------------------------------------"
-      );
-      console.log(
-        "------------------------------------------------------------------------------------------"
-      );
       /* prettier-ignore */ console.log('>>>> _ >>>> ~ file: seedManagement.js ~ line 60 ~ SeedArguments.BENEFICIARY', SeedArguments.BENEFICIARY)
+      /* prettier-ignore */ console.log('>>>> _ >>>> ~ file: seedManagement.js ~ line 67 ~ SeedArguments.ADMIN', SeedArguments.ADMIN)
+
+      // var tx = await seedFactoryInstance.populateTransaction.deploySeed(
       var tx = await seedFactoryInstance.deploySeed(
         SeedArguments.BENEFICIARY,
         SeedArguments.ADMIN,
         [SeedArguments.cUSD, SeedArguments.TUSDC],
         [SeedArguments.softCap, SeedArguments.hardCap],
         SeedArguments.price,
-        SeedArguments.startTime,
-        SeedArguments.endTime,
-        [SeedArguments.vestingDuration, SeedArguments.vestingCliff],
+        [SeedArguments.startTime, SeedArguments.endTime],
+        [SeedArguments.hardCap, SeedArguments.vestingDuration, SeedArguments.vestingCliff],
         SeedArguments.isPermissioned,
-        SeedArguments.fee,
+        [], // whitelsit
+        [SeedArguments.fee, 0, 0],
         SeedArguments.metadata
       );
+      // /* prettier-ignore */ console.log('>>>> _ >>>> ~ file: seedManagement.js ~ line 94 ~ tx', tx)
+
       // console.log(JSON.stringify(tx, null, 2))
       console.log(`Seed created through tx ${tx.hash}`);
       var receipt = await tx.wait();
       // console.log('------------------------------------------------------------------------------------------')
       // console.log('------------------------------------------------------------------------------------------')
-      // console.log(JSON.stringify(receipt, null, 2))
+      console.log(JSON.stringify(receipt, null, 2))
       // console.log("Seed deployed");
 
       var filter = seedFactoryInstance.filters.SeedCreated();
