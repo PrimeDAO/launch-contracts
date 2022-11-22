95de1562944f20336d0799adab6d764612352b1f
hdn <hieudn5293@yahoo.de>
2022-11-22 23:15:17 +0100

    chore(deploy): add tokens to contract

--- deploy/00_deploy_seed_contracts.js
+++ deploy/00_deploy_seed_contracts.js
@@ -1,5 +1,8 @@
 const { getSafeAddress } = require("../lib/params/safeAddresses");
 
+// const ONE_18_DECIMALS = 1_000000000000000000
+// const INITIAL_SUPPLY = 100000000_000000000000000000
+
 const deployFunction = async ({ getNamedAccounts, deployments, ethers }) => {
   console.log('------------------------------------------------------------------------------------------')
   const { deploy } = deployments;
@@ -58,6 +61,29 @@ const deployFunction = async ({ getNamedAccounts, deployments, ethers }) => {
     args: [],
     log: true,
   });
+
+  // Setup tokens for each account
+
+  // Hardhat default addresse
+  const Class1_User1_Address = "0x90F79bf6EB2c4f870365E785982E1f101E93b906"
+  const Class1_User2_Address = "0x15d34AAf54267DB7D7c367839AAf71A00a2C6A65"
+  const Class2_User1_Address = "0x9965507D1a55bcC2695C58ba16FB37d819B0A4dc"
+  const Class2_User2_Address = "0x976EA74026E726554dB657fA54763abd0C3a0aa9"
+  const Class3_User1_Address = "0x14dC79964da2C08b23698B3D3cc7Ca32193d9955"
+
+  const testTokenInstance = await ethers.getContract("TestToken");
+  testTokenInstance.transfer(Class1_User1_Address, "1000000000000000000000000")
+  testTokenInstance.transfer(Class1_User2_Address, "1000000000000000000000000")
+  testTokenInstance.transfer(Class2_User1_Address, "1000000000000000000000000")
+  testTokenInstance.transfer(Class2_User2_Address, "1000000000000000000000000")
+  testTokenInstance.transfer(Class3_User1_Address, "1000000000000000000000000")
+
+  const D2DInstance = await ethers.getContract("D2D");
+  D2DInstance.transfer(Class1_User1_Address, "1000000000000000000000000")
+  D2DInstance.transfer(Class1_User2_Address, "1000000000000000000000000")
+  D2DInstance.transfer(Class2_User1_Address, "1000000000000000000000000")
+  D2DInstance.transfer(Class2_User2_Address, "1000000000000000000000000")
+  D2DInstance.transfer(Class3_User1_Address, "1000000000000000000000000")
 
   console.log("---");
 };
